810da42006f2628edc34a210d74808b90ba853e8
rennegomes <101829531+rennegomes@users.noreply.github.com>
2023-10-04 13:34:23 -0300

    PWA ON

--- service-worker.js
+++ service-worker.js
@@ -9,32 +9,45 @@ self.addEventListener('install', event => {
       .then(cache => cache.addAll([
 
         './index.html',
+        './contate.html',
 
         './assets/css/fontawesome-all.min.css',
-
-        './assets/js/main.css',
+        './assets/css/main.css',
 
         './assets/js/breakpoints.min.js',
-
         './assets/js/browser.min.js',
-
         './assets/js/jquery.min.js',
-
         './assets/js/jquery.poptrox.min.js',
-
         './assets/js/main.js',
-
         './assets/js/util.js',
 
-        './icons/icone128.png',
-        './icons/icone144.png',
-        './icons/icone152.png',
-        './icons/icone167.png',
-        './icons/icone180.png',
-        './icons/icone196.png',
-        './icons/icone256.png',
-        './icons/icone512.png'
+        './images/icons/icone67.png',
+        './images/icons/icone128.png',
+        './images/icons/icone144.png',
+        './images/icons/icone152.png',
+        './images/icons/icone167.png',
+        './images/icons/icone180.png',
+        './images/icons/icone192.png',
+        './images/icons/icone196.png',
+        './images/icons/icone256.png',
+        './images/icons/icone512.png',
+
+        './assets/css/images/bg.jpg',
+        './assets/css/images/overlay.png',
         
+        './images/fulls/01.jpg',
+        './images/fulls/02.jpg',
+        './images/fulls/03.jpg',
+        './images/fulls/04.jpg',
+        './images/fulls/05.jpg',
+        './images/fulls/06.jpg',
+        
+        './images/thumbs/01.jpg',
+        './images/thumbs/02.jpg',
+        './images/thumbs/03.jpg',
+        './images/thumbs/04.jpg',
+        './images/thumbs/05.jpg',
+        './images/thumbs/06.jpg'
       ]))
   );
 });
@@ -54,16 +67,5 @@ self.addEventListener('fetch', function (event) {
       return caches.match(event.request);
     }
   }());
-
-  //Atualizacao cache
-  /*event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );*/
 
 });
